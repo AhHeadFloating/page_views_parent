843c76a596c01e8887b06d061d5c63a2a98e5c95
宋久斌 <183689751@qq.com>
2019-07-22 00:01:02 +0800

    更新数据库文件

--- database/page_views.sql
+++ database/page_views.sql
@@ -16,31 +16,25 @@
 CREATE DATABASE IF NOT EXISTS `page_views` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
 USE `page_views`;
 
--- 导出  表 page_views.blog_item 结构
-CREATE TABLE IF NOT EXISTS `blog_item` (
-  `blog_item_id` int(11) DEFAULT NULL,
-  `blog_item_name` int(11) DEFAULT NULL,
-  `blog_item_url` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='这个用户下包含的博客';
+-- 导出  表 page_views.article 结构
+CREATE TABLE IF NOT EXISTS `article` (
+  `article_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `blog_user_id` bigint(20) DEFAULT NULL,
+  `article_title` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `article_url` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`article_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=108 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='这个用户下包含的博客';
 
 -- 数据导出被取消选择。
 
 -- 导出  表 page_views.blog_user 结构
 CREATE TABLE IF NOT EXISTS `blog_user` (
-  `blog_user_id` bigint(20) DEFAULT NULL,
+  `blog_user_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `blog_user_name` varchar(50) COLLATE utf8_bin DEFAULT NULL,
   `blog_type` int(11) DEFAULT '1',
-  `create_time` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='博客用户表';
-
--- 数据导出被取消选择。
-
--- 导出  表 page_views.proxy_ip 结构
-CREATE TABLE IF NOT EXISTS `proxy_ip` (
-  `id` longtext COLLATE utf8_bin,
-  `ip` int(11) DEFAULT NULL,
-  `port` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`blog_user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='博客用户表';
 
 -- 数据导出被取消选择。
 
